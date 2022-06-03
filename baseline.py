a351847fcf6b797873fde4060f968a2f7076ea89
woithook <66987519+woithook@users.noreply.github.com>
2022-06-04 02:12:38 +0900

    GAE baseline

--- baseline.py
+++ baseline.py
@@ -1,6 +1,8 @@
 """
 A2C baseline
-Training with lamda-trace return
+Training with GAE(Generalized Advantage Estimator) for both the actor and the critic.
+delta_t =  r_t - V_t + gamma * V_t+1
+A_t = sum_0~l((gamma * lambda)^l * delta_t+l)
 """
 import gym
 import numpy as np
@@ -14,10 +16,13 @@ from models import ActorCritic
 
 import matplotlib.pyplot as plt
 
-ITERATION_NUMS = 500
-SAMPLE_NUMS = 100
+ITERATION_NUMS = 1000
+SAMPLE_NUMS = 50
 LR = 0.01
-LAMBDA = 0.99
+# Best lambda value is lower than gamma,
+# empirically lambda introduces far less bias than gamma for a reasonably accruate value function
+GAMMA = 0.99
+LAMBDA = 0.98
 CLIP_GRAD_NORM = 40
 
 
@@ -38,19 +43,19 @@ def run(random_seed):
     optim = torch.optim.Adam(agent.parameters(), lr=LR)
     scheduler = torch.optim.lr_scheduler.LinearLR(optim, start_factor=1.0, end_factor=0.0, total_iters=ITERATION_NUMS)
 
-    gamma = 0.99
+    gamma = GAMMA
 
     iterations = []
     test_results = []
 
     init_state = task.reset()
     for i in range(ITERATION_NUMS):
-        states, actions, returns, current_state = roll_out(agent, task, SAMPLE_NUMS, init_state, gamma)
+        states, actions, advs, v_targets, current_state = roll_out(agent, task, SAMPLE_NUMS, init_state, gamma)
         init_state = current_state
-        train(agent, optim, scheduler, states, actions, returns, ACTION_DIM)
+        train(agent, optim, scheduler, states, actions, advs, v_targets, ACTION_DIM)
 
         # testing
-        if (i + 1) % 10 == 0:
+        if (i + 1) % (ITERATION_NUMS // 10) == 0:
             result = test(gym_name, agent)
             print("iteration:", i + 1, "test result:", result / 10.0)
             iterations.append(i + 1)
@@ -63,44 +68,64 @@ def roll_out(agent, task, sample_nums, init_state, gamma):
     states = []
     actions = []
     rewards = []
-    vs = []
+    v_t_s = []
+    v_t1_s = []
+    dones = []
     state = init_state
 
     for i in range(sample_nums):
         states.append(state)
-        with torch.no_grad():
-            logits, _ = agent(torch.Tensor(state))
-            act_probs = F.softmax(logits, dim=-1)
-            m = Categorical(act_probs)
-            act = m.sample()
-            actions.append(act)
+        act, v_t = choose_action(agent, state)
+        actions.append(act)
 
         next_state, reward, done, _ = task.step(act.numpy())
-        _, v_t_1 = agent(torch.Tensor(next_state))
+        with torch.no_grad():
+            _, v_t1 = agent(torch.Tensor(next_state))
 
         rewards.append(reward)
-        vs.append(v_t_1.detach().numpy())
+        v_t = v_t.detach().numpy()
+        v_t1 = v_t1.detach().numpy()
+        v_t_s.append(v_t)
+        v_t1_s.append(v_t1)
+        dones.append(1 if done is False else 0)
+
         state = next_state
         if done:
-            state = task.reset()  # fatal bug happened
+            state = task.reset()
             break
+    advs, v_targets = gae(rewards, v_t_s, v_t1_s, dones, gamma, LAMBDA)
 
-    return states, actions, calculate_returns(rewards, vs, gamma), state
+    return states, actions, advs, v_targets, state
 
 
-def calculate_returns(rewards, vs, gamma):
+def gae(rewards, v_t_s, v_t1_s, dones, gamma, lambda_):
+    batch_size = len(rewards)
+    advs = np.zeros(batch_size + 1)
+    for t in reversed(range(0, batch_size)):
+        delta = rewards[t] - v_t_s[t] + (gamma * v_t1_s[t] * dones[t])
+        advs[t] = delta + (gamma * lambda_ * advs[t+1] * dones[t])
+    value_target = advs[:batch_size] + np.squeeze(v_t_s)  # target v is calculated from adv.
+
+    return advs[:batch_size], value_target
+
+
+def calculate_qs(rewards, final_v, gamma):
     returns = np.zeros_like(rewards)
-    R = 0
+    R = final_v
     for t in reversed(range(0, len(rewards))):
-        R = rewards[t] + gamma * (1 - LAMBDA) * vs[t] + gamma * LAMBDA * R
+        R = R * gamma + rewards[t]
         returns[t] = R
     return returns
 
 
-def train(agent, optim, scheduler, states, actions, returns, action_dim):
+def train(agent, optim, scheduler, states, actions, advs, v_targets, action_dim):
     states = torch.Tensor(np.array(states))
     actions = torch.tensor(actions, dtype=torch.int64).view(-1, 1)
-    returns = torch.Tensor(returns)
+
+    v_targets = torch.Tensor(v_targets).detach()
+    # v_targets = (v_targets - advs.mean()) / (v_targets.std(unbiased=False) + 1e-12)
+    advs = torch.Tensor(advs).detach()
+    # advs = (advs - advs.mean()) / (advs.std(unbiased=False) + 1e-12)
 
     logits, v = agent(states)
     logits = logits.view(-1, action_dim)
@@ -109,17 +134,11 @@ def train(agent, optim, scheduler, states, actions, returns, action_dim):
     log_probs = F.log_softmax(logits, dim=1)
     log_probs_act = log_probs.gather(1, actions).view(-1)
 
-    q = returns.detach()
-    q = (q - q.mean()) / (q.std(unbiased=False) + 1e-12)
-    a = q - v
-    a = (a - a.mean()) / (a.std(unbiased=False) + 1e-12)
-
-    loss_policy = - (a.detach() * log_probs_act).mean()
-    # loss_critic = a.pow(2.).sum()
-    loss_critic = F.mse_loss(q, v, reduction='mean')
+    loss_policy = - (advs * log_probs_act).mean()
+    loss_critic = F.mse_loss(v_targets, v, reduction='mean')
     loss_entropy = - (log_probs * probs).mean()
 
-    loss = loss_policy + .5 * loss_critic - .001 * loss_entropy
+    loss = loss_policy + .25 * loss_critic - .001 * loss_entropy
     optim.zero_grad()
     loss.backward()
     torch.nn.utils.clip_grad_norm_(agent.parameters(), CLIP_GRAD_NORM)
@@ -133,17 +152,23 @@ def test(gym_name, agent):
     for test_epi in range(10):
         state = test_task.reset()
         for test_step in range(500):
-            with torch.no_grad():
-                logits, _ = agent(torch.Tensor(state))
-                act_probs = F.softmax(logits, dim=-1)
-                m = Categorical(act_probs)
-                act = m.sample()
+            act, _ = choose_action(agent, state)
             next_state, reward, done, _ = test_task.step(act.numpy())
             result += reward
             state = next_state
             if done:
                 break
     return result
+
+
+@torch.no_grad()
+def choose_action(agent, state):
+    logits, v = agent(torch.Tensor(state))
+    act_probs = F.softmax(logits, dim=-1)
+    m = Categorical(act_probs)
+    act = m.sample()
+
+    return act, v
 
 
 if __name__ == '__main__':
@@ -153,5 +178,5 @@ if __name__ == '__main__':
         test_results = run(random_seed)
         total_test_results.append(test_results)
 
-    dir = 'learning_results' + date + '.npy'
+    dir = 'runs/Baseline/learning_results' + date + '.npy'
     np.save(dir, total_test_results)
